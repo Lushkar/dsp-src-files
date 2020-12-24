48c37234bd9b77e2467c9663dd1684b54b8d090f
Abdul Ahad <ahadbutt.pk@gmail.com>
2020-12-24 20:05:46 +0500

    Made a ZOH function

--- assignment-2/zero_order_hold.m
+++ assignment-2/zero_order_hold.m
@@ -1,9 +1,12 @@
 function y = zero_order_hold (x, T)
-%y = zeros(length(x));
+y = [];
 y = 0;
 for i = 1:T:length(x)
-  sprintf('%d - %d - %d', i, i + T, x(i));
-  
+  % sprintf('%d - %d - %d', i, i + T, x(i))
+  % size(x(i))
+  % size(1, ones(T))
+  y(i:i + T - 1) = x(i) .* ones(1, T);
 end   
 
+y = transpose(y);
 endfunction
